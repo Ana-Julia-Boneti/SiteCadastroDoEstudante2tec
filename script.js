ab8e6f9f979c8872450fe2c064572cae2c44a907
Ana Julia Boneti Volgue <ana.volgue@escola.pr.gov.br>
2026-05-04 19:19:36 +0000

    11

--- script.js
+++ script.js
@@ -6,10 +6,11 @@ form.addEventListener('submit', function(event){
 
     const nome = document.getElementById('nomeAluno').value;
     const idade = document.getElementById('idadeAluno').value;
-     const cpf = document.getElementById('idadecpf').value;
+    const cpf = document.getElementById('cpfAluno').value;
+    const email = document.getElementById('emailAluno').value;
 
     const novoItem = document.createElement('li');
-    novoItem.innerHTML = `<strong>${nome}</strong> - ${idade} anos - ${cpf}`;
+    novoItem.innerHTML = `<strong>${nome}</strong> - ${idade} anos - ${cpf} - ${email}`;
 
     lista.appendChild(novoItem);
 
